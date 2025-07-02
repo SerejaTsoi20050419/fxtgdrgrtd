3c044c054ca791885502e5d002eb882580ddf1dd
SerejaTsoi20050419 <mistendo12345@gmail.com>
2025-07-02 19:18:39 +0900

    first commit

---
+++ .history/src/js/index_20250702165242.js
@@ -0,0 +1,57 @@
+const initSwipers = () => {
+  const brandSwiper = new Swiper('#more-brands', {
+    slidesPerView: 'auto',
+    spaceBetween: 16,
+    pagination: {
+      el: '#more-brands .swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      768: {
+        enabled: false
+      }
+    }
+  });
+
+  const deviceSwiper = new Swiper('#more-devices', {
+    slidesPerView: 'auto',
+    spaceBetween: 16,
+    pagination: {
+      el: '#more-devices .swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      768: {
+        enabled: false
+      }
+    }
+  });
+
+ const swiper = new Swiper('#swiper-services', {
+  slidesPerView: 'auto',
+  spaceBetween: 16,
+  pagination: {
+    el: '#swiper-services .swiper-pagination',
+    clickable: true,
+  },
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  initSwipers();
+
+  // 2. Кнопка "Читать далее"
+  const readMoreBtn = document.querySelector('#more-content .more__button');
+  const moreText = document.querySelector('#more-content .more__content');
+  const expandIcon = readMoreBtn.querySelector('.more__image');
+  const expandText = readMoreBtn.querySelector('.more__text');
+
+  readMoreBtn.addEventListener('click', () => {
+    const isExpanded = moreText.classList.contains('more__content--expanded');
+    moreText.classList.toggle('more__content--expanded');
+    moreText.classList.toggle('more__content--shrinked');
+
+    expandIcon.classList.toggle('more__image--expand');
+    expandText.textContent = isExpanded ? 'Читать далее' : 'Скрыть';
+  });
+}
+});
